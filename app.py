8e0f2b85f830439f8d04adc2fd01c6cfa5872da8
benkhong27 <143551083+benkhong27@users.noreply.github.com>
2024-10-10 01:27:25 +0000

    Added team page for Ben, Newton and Khiem

--- app.py
+++ app.py
@@ -121,3 +121,7 @@ def team_jin_allison_saad():
 @app.route("/brian_jia_honghao")
 def team_brian_jia_honghao():
     return render_template('teams/brian_jia_honghao.html')
+
+@app.route("/ben_newton_khiem")
+def team_ben_newton_khiem():
+    return render_template('teams/ben_newton_khiem.html')
