aac870ea166423b161af98a006ef0d287b1311be
ThienAnTruong(Andrew) <166311237+AndrewTr0612@users.noreply.github.com>
2025-11-07 02:56:47 +0000

    init the team html file and add Andrew intro

--- app.py
+++ app.py
@@ -414,3 +414,7 @@ def team_41():
 @app.route("/team_jose_caden_wanni")
 def team_jose_caden_wanni():
     return render_template('teams/team_jose_caden_wanni.html')
+
+@app.route("/an_princeton_nathan_anthony")
+def team_an_princeton_nathan_anthony():
+    return render_template('teams/an_princeton_nathan_anthony.html')
